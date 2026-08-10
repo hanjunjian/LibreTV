365f04117f3cba7741e1c63230ad2e027e66532b
hanjunjian <hanjunjian@users.noreply.github.com>
2026-08-10 19:01:05 +0800

    fix: restore full proxy code, cleanup debug files

--- functions/proxy/index.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-    return new Response(JSON.stringify({ status: "proxy_index", url: context.request.url }), {
-        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }
-    });
-}
